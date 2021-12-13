ebd426d9399b70637a4acbeae93f6088aaa19c3f
diegoLince <diego94_cr@hotmail.com>
2021-12-13 20:16:51 -0300

    Add files via upload

---
+++ Component.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("fiori.template.Component", {
+        //Implementação customizada
+
+        metadata : {
+            manifest : "json"
+        },
+
+        init : function(){
+
+            //Chama super Classe
+            UIComponent.prototype.init.apply(this, arguments);
+
+            this.getRouter().initialize();
+            
+        },
+	});
+});
---
+++ index.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+
+"sap/ui/core/mvc/XMLView",
+"sap/ui/core/ComponentContainer"
+    
+], function(XMLView, ComponentContainer) { //Os nomes são de acordo com a ordem
+
+    'use trict';
+
+   // XMLView.create({
+     //   viewName : "fiori.template.view.Worklist"
+    //}).then(function (oView){
+     //   oView.placeAt("content");
+    //});
+
+    new ComponentContainer({
+        name : "fiori.template",
+        settings : {
+            id : "template"
+        },
+        async : true
+    }).placeAt("content");
+});
